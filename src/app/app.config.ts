0d59aac3fcbf965bc8ec46949695171a5e50091c
Felipe0331 <felipecanocalle@gmail.com>
2024-06-28 15:41:48 -0500

    login y register listos

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,8 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+// import { routes } from './app.module' ;
+
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
