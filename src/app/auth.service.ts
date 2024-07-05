71fcdb700368294b18e11567c924142b892289d6
Yoiner Moreno <yoinerdeyhan@gmail.com>
2024-07-05 12:46:59 -0500

    🎨 Deleting unnecesary comments

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:8080/api'; // Asegúrate de que esta URL sea correcta
+  private apiUrl = 'http://localhost:8080/api';
 
   constructor(private http: HttpClient) { }
 
